273f628fafcc7c686ed6128b0a89bdddb5a47e30
Yaron Efrat <yaronefrat92@gmail.com>
2023-01-17 21:56:03 +0100

    Change config.tsx to reflect my details

--- src/App/config.tsx
+++ src/App/config.tsx
@@ -4,37 +4,37 @@ import { Email, GitHub, LinkedIn, Resume } from 'icons';
 
 const config: Config = {
   name: {
-    display: 'Adam Alston',
-    aria: 'My name is Adam Alston',
+    display: 'Yaron Efrat',
+    aria: 'My name is Yaron Efrat',
   },
   title: {
-    display: 'Software Engineer',
-    aria: 'I am a software engineer',
+    display: 'Physicist => Software Developer',
+    aria: 'I am a physicist turned software developer',
   },
   buttons: [
     {
       display: 'GitHub',
       aria: 'Visit my GitHub profile',
       icon: <GitHub />,
-      href: 'https://github.com/adamalston/',
+      href: 'https://github.com/yaron-E92/',
     },
     {
       display: 'LinkedIn',
       aria: 'Visit my LinkedIn profile',
       icon: <LinkedIn />,
-      href: 'https://www.linkedin.com/in/adam-alston/',
+      href: 'https://www.linkedin.com/in/yaron-efrat-351472176/',
     },
     {
       display: 'Resume',
       aria: 'View my resume in Google Drive',
       icon: <Resume />,
-      href: 'https://drive.google.com/drive/folders/10k8NWflSYQ5laPzuWtK3bzUKzuOeas8i/',
+      href: 'https://drive.google.com/file/d/1wKpMuVcNXvDz2HrhpLvbIcjrd1mRwleq/view?usp=share_link',
     },
     {
       display: 'Email',
       aria: 'Send me an email',
       icon: <Email />,
-      href: 'mailto:aalston9@gmail.com',
+      href: 'mailto:yaronefrat92@gmail.com',
     },
   ],
 };
